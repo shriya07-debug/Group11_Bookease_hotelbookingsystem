cc65d3e25e65f5c9b375810a57afc8ccda7930e3
shriya07-debug <shriyaawale2007@gmail.com>
2025-12-12 22:17:15 +0545

    third commit

--- src/dao/HotelDAO.java
+++ src/dao/HotelDAO.java
@@ -4,10 +4,53 @@
  */
 package dao;
 
+import database.MySqlConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Hotel;
+
 /**
  *
  * @author sailenawale
  */
 public class HotelDAO {
+    MySqlConnection mysql = new MySqlConnection();
     
+    public void signup(Hotel dashboard){
+        Connection conn = mysql.openConnection();
+        String sql = "insert into dashboard (hotelid, name, location, description, rating, image) values (?,?,?,?,?,?)";
+        try(PreparedStatement pstm = conn.prepareStatement(sql)){
+            pstm.setInt(1, dashboard.getHotelid());
+            pstm.setString(2, dashboard.getName());
+            pstm.setString(3, dashboard.getLocation());
+             pstm.setString(4, dashboard.getDescription());
+              pstm.setDouble(5, dashboard.getRating());
+               pstm.setString(6, dashboard.getImage());
+            pstm.executeUpdate();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }finally{
+            mysql.closeConnection(conn);
+        }
+    }
+
+
+public boolean check(Hotel search){
+        Connection conn = mysql.openConnection();
+        String sql = "select * from users where email = ? or username = ?";
+        try(PreparedStatement pstm = conn.prepareStatement(sql)){
+            pstm.setString(1, dashboard.getEmail());
+            pstm.setString(2, dashboard.getUsername());
+            ResultSet result = pstm.executeQuery();
+            return result.next();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }finally{
+            mysql.closeConnection(conn);
+        }
+        return false;
+    }
 }
+
+
